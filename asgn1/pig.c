5c919ca847d065e52cb95ac12c6978b4e25b6436
Aiden Trager <astrager@ucsc.edu>
2023-10-14 18:26:09 +0000

    modified

--- asgn1/pig.c
+++ asgn1/pig.c
@@ -1,10 +1,10 @@
+#include "names.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "names.h"
-typedef enum {SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER} Position;
+typedef enum { SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER } Position;
 
 const Position pig[7] = { SIDE, SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER, JOWLER };
-
 
 int main(void) {
     // Declaration of num_players
@@ -28,14 +28,14 @@ int main(void) {
     int turn = 0;
     int roll = 10;
     int i = 0;
-    #define SEED seed
+#define SEED seed
     srandom(SEED);
     int scores[num_players];
-    for (int j = 0; j<num_players; j+=1){
-        scores[j]=0;
+    for (int j = 0; j < num_players; j += 1) {
+        scores[j] = 0;
     }
     int amount_gained = 0;
-   
+
     while (win == 0) {
         i = 0;
         while (i < num_players) {
@@ -44,40 +44,39 @@ int main(void) {
             while (turn == 0) {
                 roll = random() % 7;
                 switch (pig[roll]) {
-                    case SIDE:
-                        turn = 1;
-                        amount_gained = 0;
-                        scores[i] += amount_gained;
-                        printf("Rolls %d has %d\n", amount_gained, scores[i]);
-                        break;
-                    case RAZORBACK:
-                        amount_gained = 10;
-                        scores[i] += amount_gained;
-                        printf("Rolls %d has %d\n", amount_gained, scores[i]);
-                        break;
-                    case TROTTER:
-                        amount_gained = 10;
-                        scores[i] += amount_gained;
-                        printf("Rolls %d has %d\n", amount_gained, scores[i]);
-                        break;
-                    case SNOUTER:
-                        amount_gained = 15;
-                        scores[i] += amount_gained;
-                        printf("Rolls %d has %d\n", amount_gained, scores[i]);
-                        break;
-                    case JOWLER:
-                        amount_gained = 5;
-                        scores[i] += amount_gained;
-                        printf("Rolls %d has %d\n", amount_gained, scores[i]);
-                        break;
+                case SIDE:
+                    turn = 1;
+                    amount_gained = 0;
+                    scores[i] += amount_gained;
+                    printf(" rolls %d, has %d\n", amount_gained, scores[i]);
+                    break;
+                case RAZORBACK:
+                    amount_gained = 10;
+                    scores[i] += amount_gained;
+                    printf(" rolls %d, has %d\n", amount_gained, scores[i]);
+                    break;
+                case TROTTER:
+                    amount_gained = 10;
+                    scores[i] += amount_gained;
+                    printf(" rolls %d, has %d\n", amount_gained, scores[i]);
+                    break;
+                case SNOUTER:
+                    amount_gained = 15;
+                    scores[i] += amount_gained;
+                    printf(" rolls %d, has %d\n", amount_gained, scores[i]);
+                    break;
+                case JOWLER:
+                    amount_gained = 5;
+                    scores[i] += amount_gained;
+                    printf(" rolls %d, has %d\n", amount_gained, scores[i]);
+                    break;
                 }
                 if (scores[i] >= 100) {
                     printf("%s Won!\n", player_name[i]);
-			i = 11;
+                    i = 11;
                     win = 1;
                     turn = 1;
                 }
-               
             }
             i += 1;
         }
