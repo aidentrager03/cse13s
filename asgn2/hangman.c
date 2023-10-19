71c5dea1ebb2d6a4c287cdc5ad35361f71496cb1
Aiden Trager <astrager@ucsc.edu>
2023-10-19 00:41:56 +0000

    adding files

---
+++ asgn2/hangman.c
@@ -0,0 +1,86 @@
+#include "hangman_helpers.h"
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("wrong number of arguments\n");
+        fprintf(stderr, "usage: %s <secret_phrase>\n", argv[0]);
+        printf("if the secret is multiple words, you must quote it\n");
+        return 1;
+    }
+
+    const char* secret = argv[1];
+    char eliminated[MAX_LENGTH] = "";
+    int mistakes = 0;
+    bool clear = true;
+    // Validate secret
+    if (!validate_secret(secret)) {
+        fprintf(stderr, "the secret phrase must contain only lowercase letters, spaces, hyphens, and apostrophes\n");
+        return 1;
+    }
+    
+    size_t remaining_letters = 0;
+    for (size_t z = strlen(secret); z > 0; z--) {
+        if ('a' <= secret[z - 1] && secret[z - 1] <= 'z') {            
+        remaining_letters++;
+    }
+    }
+    char guessed[MAX_LENGTH] = "";  // Store guessed letters
+    size_t eliminated_length = 0;
+
+    while (mistakes < LOSING_MISTAKE) {
+        // Print game state
+        if (clear){
+        print_game_state(secret, eliminated, mistakes);
+        }
+        // Prompt for a letter
+        printf("Guess a letter: ");
+        char guess = read_letter();
+
+        // Check if the letter has already been guessed
+        if (string_contains_character(guessed, guess) || !(guess >= 'a' && guess <= 'z')) {
+            clear = false;
+            continue;  // Skip the rest of the loop iteration
+        }
+        clear = true;
+        guessed[strlen(guessed)] = guess;
+        guessed[strlen(guessed)] = '\0';
+
+        // Check if the letter is in the secret
+        const char* secret_ptr = secret;  // Create a separate pointer
+        size_t letter_occurrences = 0;    // Count occurrences of the guessed letter
+
+        while (*secret_ptr != '\0') {
+            if (*secret_ptr == guess) {
+                remaining_letters--;
+                letter_occurrences++;
+            }
+            secret_ptr++;
+        }
+
+        if (letter_occurrences == 0) {
+            mistakes++;
+        }
+
+        // Add the letter to eliminated
+        eliminated[eliminated_length] = guess;
+        eliminated_length++;
+        eliminated[eliminated_length] = '\0';
+
+        // Check if the player has won
+        if (remaining_letters == 0) {
+            print_game_state(secret, eliminated, mistakes);
+            printf("You win! The secret phrase was: %s\n", secret);
+
+            // Exit the program as the player has won
+            return 0;
+        }
+    }
+
+    // Player loses
+    if (mistakes == LOSING_MISTAKE) {
+        print_game_state(secret, eliminated, mistakes);
+        printf("You lose! The secret phrase was: %s\n", secret);
+    }
+
+    return 0;
+}
---
+++ asgn2/hangman_helpers.c
@@ -0,0 +1,69 @@
+#include "hangman_helpers.h"
+
+bool is_lowercase_letter(char c) {
+    if (c >= 'a' && c <= 'z'){
+        return true;}
+    else{
+        return false;}
+}
+
+
+bool validate_secret(const char* secret) {
+    if (strlen(secret) > 256) {
+        printf("Secret is too long (max 256 characters).\n");
+        return false;
+    }
+    while (*secret != '\0') {
+        if (!is_lowercase_letter(*secret) && *secret != ' ' && *secret != '\'' && *secret != '-') {
+            printf("invalid character: '%c'\n", *secret);
+            return false;
+        }
+
+        secret++;
+    }
+    return true;
+}
+
+
+bool string_contains_character(const char* s, char c) {
+    while (*s != '\0') {
+        if (*s == c) {
+            return true;
+        }
+        s++;
+    }
+    return false;
+}
+
+char read_letter(void) {
+    char input;
+    scanf(" %c", &input);
+    return input;
+}
+
+void print_game_state(const char* phrase, const char* eliminated, int mistakes) {
+    printf("%s\n", CLEAR_SCREEN);
+
+    // Print gallows
+    if (mistakes < LOSING_MISTAKE) {
+        printf("%s", arts[mistakes]);
+    } else {
+        printf("%s", arts[LOSING_MISTAKE]);
+    }
+
+    // Print phrase
+    printf("Phrase: ");
+    while (*phrase != '\0') {
+        if (string_contains_character(punctuation, *phrase) || string_contains_character(eliminated, *phrase)) {
+            printf("%c", *phrase);
+        } else if (*phrase == ' ') {
+            printf(" "); // Display spaces in the phrase
+        } else {
+            printf("_");
+        }
+        phrase++;
+    }
+
+    // Print eliminated letters
+    printf("\nEliminated: %s\n\n", eliminated);
+}
