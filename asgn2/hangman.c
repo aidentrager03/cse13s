2110fd9d6b9bd64f2b2d40764edc22c858f8ff54
Aiden Trager <astrager@ucsc.edu>
2023-10-23 21:19:43 +0000

    n

--- asgn2/hangman.c
+++ asgn2/hangman.c
@@ -64,20 +64,12 @@ int main(int argc, char *argv[]) {
 
         if (letter_occurrences == 0) {
             mistakes++;
-            // Shift all characters in 'eliminated' to the right
-            for (size_t i = eliminated_length; i > 0; i--) {
-                eliminated[i] = eliminated[i - 1];
-            }
-            eliminated[0] = guess; // Insert the new character at the beginning
+            eliminated[eliminated_length] = guess;
             eliminated_length++;
             eliminated[eliminated_length] = '\0';
         } else {
-            // Similar operation for 'correct_guesses'
-            for (size_t i = strlen(correct_guesses); i > 0; i--) {
-                correct_guesses[i] = correct_guesses[i - 1];
-            }
-            correct_guesses[0] = guess;
-            correct_guesses[strlen(correct_guesses) + 1] = '\0';
+            correct_guesses[strlen(correct_guesses)] = guess;
+            correct_guesses[strlen(correct_guesses)] = '\0';
         }
 
         // Check if the player has won
