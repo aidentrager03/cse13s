4bd66c647bcbcddcecf26038face321c3a457e94
Aiden Trager <astrager@ucsc.edu>
2023-11-09 18:09:04 +0000

    new

---
+++ asgn5/path.c
@@ -0,0 +1,73 @@
+#include "path.h"
+
+#include "graph.h"
+#include "stack.h"
+
+#include <stdlib.h>
+
+typedef struct path {
+    uint32_t total_weight;
+    Stack *vertices;
+} Path;
+
+Path *path_create(uint32_t capacity) {
+    Path *p = (Path *) malloc(sizeof(Path));
+    p->total_weight = 0;
+    p->vertices = stack_create(capacity);
+    return p;
+}
+
+void path_free(Path **pp) {
+    if (pp != NULL && *pp != NULL) {
+        stack_free(&((*pp)->vertices));
+        free(*pp);
+        *pp = NULL;
+    }
+}
+
+uint32_t path_vertices(const Path *p) {
+    return stack_size(p->vertices);
+}
+
+uint32_t path_distance(const Path *p) {
+    return p->total_weight;
+}
+
+void path_add(Path *p, uint32_t val, const Graph *g) {
+    uint32_t last_vertex;
+    if (stack_pop(p->vertices, &last_vertex)) {
+        p->total_weight += graph_get_weight(g, last_vertex, val);
+    }
+    stack_push(p->vertices, val);
+}
+
+uint32_t path_remove(Path *p, const Graph *g) {
+    uint32_t removed_vertex;
+    if (stack_pop(p->vertices, &removed_vertex)) {
+        uint32_t last_vertex;
+        if (stack_pop(p->vertices, &last_vertex)) {
+            p->total_weight -= graph_get_weight(g, last_vertex, removed_vertex);
+        } else {
+            p->total_weight = 0;
+        }
+    }
+    return removed_vertex;
+}
+
+void path_copy(Path *dst, const Path *src) {
+    stack_copy(dst->vertices, src->vertices);
+    dst->total_weight = src->total_weight;
+}
+
+void path_print(const Path *p, FILE *f, const Graph *g) {
+    for (uint32_t i = 0; i < stack_size(p->vertices); i++) {
+        uint32_t path_item;
+        if (stack_peek(p->vertices, &path_item)) {
+            fprintf(f, "%s", graph_get_vertex_name(g, path_item));
+            if (i < stack_size(p->vertices) - 1) {
+                fprintf(f, " -> ");
+            }
+        }
+    }
+    fprintf(f, "\n"); // Add a newline at the end of the path.
+}
---
+++ asgn5/tsp.c
@@ -0,0 +1,198 @@
+#include "graph.h"
+#include "path.h"
+#include "stack.h"
+
+#include <getopt.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Path *solve_tsp(const Graph *graph);
+
+int next_permutation(uint32_t *array, uint32_t length);
+
+int main(int argc, char *argv[]) {
+    int directed = 0;
+    char *input_file = NULL;
+    char *output_file = NULL;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "dhi:o:")) != -1) {
+        switch (opt) {
+        case 'd': directed = 1; break;
+        case 'i': input_file = optarg; break;
+        case 'o': output_file = optarg; break;
+        case 'h':
+            printf("Usage: %s [options]\n", argv[0]);
+            printf("Options:\n");
+            printf("  -d                Specify the graph to be directed.\n");
+            printf("  -i input_file     Specify the input file path containing the cities and "
+                   "edges of a graph.\n");
+            printf("  -o output_file    Specify the output file path to print to.\n");
+            printf("  -h                Print out a help message describing the purpose of the "
+                   "program and the command-line options it accepts, exiting the program "
+                   "afterwards.\n");
+            return 0;
+        default:
+            // read from stdin as a default
+            fprintf(stderr, "Invalid option\n");
+            return 1;
+        }
+    }
+    FILE *input;
+    if (input_file == NULL) {
+        input = stdin;
+    } else {
+        input = fopen(input_file, "r");
+    }
+    if (input == NULL) {
+        fprintf(stderr, "Failed to open the input file.\n");
+        return 1;
+    }
+
+    uint32_t num_vertices;
+    fscanf(input, "%" SCNu32, &num_vertices);
+    printf("%" SCNu32, num_vertices);
+    Graph *graph = graph_create(num_vertices, directed);
+
+    char vertex_name[100];
+    for (uint32_t i = 0; i < num_vertices; i++) {
+        fscanf(input, "%s\n", vertex_name);
+        printf("%s\n", vertex_name);
+        graph_add_vertex(graph, vertex_name, i);
+    }
+
+    uint32_t num_edges;
+    fscanf(input, "%" SCNu32, &num_edges);
+    printf("edge: %u", num_edges);
+    for (uint32_t i = 0; i < num_vertices; i++) {
+        printf("Vertex %d: %s\n", i, graph_get_vertex_name(graph, i));
+    }
+
+    for (uint32_t i = 0; i < num_edges; i++) {
+        uint32_t start, end, weight;
+        fscanf(input, "%" SCNu32 " %" SCNu32 " %" SCNu32, &start, &end, &weight);
+        printf("Edge: %u -> %u, Weight: %u\n", start, end, weight);
+    }
+
+    for (uint32_t i = 0; i < num_edges; i++) {
+        uint32_t start, end, weight;
+        fscanf(input, "%" SCNu32 " %" SCNu32 " %" SCNu32, &start, &end,
+            &weight); // Use SCNu32 to read uint32_t.
+        printf("%u %u %u", start, end, weight);
+        graph_add_edge(graph, start, end, weight);
+        graph_print(graph);
+    }
+
+    // Close the input file.
+    fclose(input);
+
+    // Solve the TSP problem using brute force.
+    Path *tsp_path = solve_tsp(graph);
+
+    // Print the TSP solution.
+    if (output_file != NULL) {
+        FILE *output = fopen(output_file, "w");
+        if (output != NULL) {
+            fprintf(output, "Alissa starts at:\n");
+            path_print(tsp_path, output, graph);
+            fprintf(output, "Total Distance: %u\n", path_distance(tsp_path));
+            fclose(output);
+        }
+    } else {
+        printf("Alissa starts at:\n");
+        path_print(tsp_path, stdout, graph);
+        printf("Total Distance: %u\n", path_distance(tsp_path));
+    }
+
+    // Free memory.
+    path_free(&tsp_path);
+    graph_free(&graph);
+
+    return 0;
+}
+
+Path *solve_tsp(const Graph *graph) {
+    uint32_t num_vertices = graph_vertices(graph);
+
+    uint32_t *vertices = (uint32_t *) malloc(num_vertices * sizeof(uint32_t));
+    for (uint32_t i = 0; i < num_vertices; i++) {
+        vertices[i] = i;
+    }
+
+    uint32_t best_weight = UINT32_MAX;
+    Path *best_path = path_create(num_vertices);
+
+    for (uint32_t i = 0; i < num_vertices; i++) {
+        path_add(best_path, vertices[i], graph);
+    }
+
+    do {
+        uint32_t current_weight = 0;
+
+        for (uint32_t i = 0; i < num_vertices; i++) {
+            current_weight
+                += graph_get_weight(graph, vertices[i], vertices[(i + 1) % num_vertices]);
+        }
+
+        // Add the weight of the last edge.
+        current_weight += graph_get_weight(graph, vertices[num_vertices - 1], vertices[0]);
+
+        // Debug print statements
+        printf("Permutation: ");
+        for (uint32_t i = 0; i < num_vertices; i++) {
+            printf("%u ", vertices[i]);
+        }
+        printf("Weight: %u\n", current_weight);
+
+        if (current_weight < best_weight) {
+            best_weight = current_weight;
+
+            while (path_vertices(best_path) > 0) {
+                path_remove(best_path, graph);
+            }
+
+            for (uint32_t i = 0; i < num_vertices; i++) {
+                path_add(best_path, vertices[i], graph);
+            }
+        }
+    } while (next_permutation(vertices, num_vertices));
+
+    free(vertices);
+
+    return best_path;
+}
+
+int next_permutation(uint32_t *array, uint32_t length) {
+    // Find the largest index k such that a[k] < a[k+1].
+    int k = (int) (length - 2); // Cast to int to address signedness issue.
+    while (k >= 0 && array[k] >= array[k + 1]) {
+        k--;
+    }
+
+    if (k < 0) {
+        return 0;
+    }
+
+    int l = (int) (length - 1);
+    while (array[l] <= array[k]) {
+        l--;
+    }
+
+    uint32_t temp = array[k];
+    array[k] = array[l];
+    array[l] = temp;
+
+    int i = (int) (k + 1);
+    int j = (int) (length - 1);
+    while (i < j) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+    }
+
+    return 1;
+}
