12aba198e231eb4b85764fe3db598d2d3485b2de
Aiden Trager <astrager@ucsc.edu>
2023-11-29 05:59:25 +0000

    n

--- asgn7/bad_xd.c
+++ asgn7/bad_xd.c
@@ -5,8 +5,7 @@
 
 #define B 16
 
-void p(const char *b, size_t s, size_t o) {
-    printf("%08zx: ", o);
+void p(const char *b, size_t s) {
     for (size_t i = 0; i < B; ++i)
         printf(i < s ? "%02x%c" : "  ", (unsigned char) b[i], i % 2 == 1 ? ' ' : ' ');
     printf(" ");
@@ -21,24 +20,22 @@ void f(const char *f) {
         exit(EXIT_FAILURE);
 
     char b[B];
-    size_t o = 0;
     ssize_t r1 = 1;
 
     while (r1 > 0) {
         ssize_t r = 0;
         while (r != B) {
-            ssize_t read_result = read(d, &b[r], (size_t) (B - r));
-            if (read_result < 0) {
+            r1 = read(d, &b[r], (size_t) (B - r));
+            if (r1 < 0) {
                 perror("Error reading file");
                 exit(EXIT_FAILURE);
             }
-            r += (size_t) read_result;
-            if (read_result == 0)
+            r += (size_t) r1;
+            if (r1 == 0)
                 break;
         }
         if (r > 0)
-            p(b, (size_t) r, o);
-        o += r > 0 ? (size_t) r : 0;
+            p(b, (size_t) r);
     }
 
     if (f)
