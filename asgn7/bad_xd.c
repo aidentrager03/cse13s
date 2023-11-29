ab2928d52f281f910a25981df0f534bc04abe505
Aiden Trager <astrager@ucsc.edu>
2023-11-29 07:07:34 +0000

    n

--- asgn7/bad_xd.c
+++ asgn7/bad_xd.c
@@ -8,52 +8,48 @@
 void p(const char *b, size_t s, size_t o) {
     printf("%08zx: ", o);
 
-    for (size_t i = 0; i < B; ++i) {
-        printf(i < s ? "%02x%c" : "  ", (unsigned char) b[i], i % 2 == 1 ? ' ' : ' ');
-    }
+    for (size_t i = 0; i < s; ++i)
+        printf("%02x%c", (unsigned char) b[i], i % 2 == 1 ? ' ' : ' ');
 
-    for (size_t i = 0; i < B; ++i) {
-        putchar(i < s && (b[i] >= 32 && b[i] <= 126) ? b[i] : '.');
-    }
+    for (size_t i = s; i < B; ++i)
+        printf(i % 2 == 1 ? "  " : " ");
+
+    printf(" ");
+
+    for (size_t i = 0; i < s; ++i)
+        putchar((b[i] >= 32 && b[i] <= 126) ? b[i] : '.');
 
     printf("\n");
 }
 
-void f(const char *f) {
+void h(const char *f) {
     int d = f ? open(f, 00) : 0;
-    if (d == -1) {
+    if (d == -1)
         exit(1);
-    }
 
-    char b[B];
-    size_t o = 0;
-    ssize_t r1 = 1;
+    char U[B];
+    size_t O = 0;
+    ssize_t R = 1;
 
-    while (r1 > 0) {
+    while (R > 0) {
         ssize_t r = 0;
         while (r != B) {
-            r1 = read(d, &b[r], (size_t) (B - r));
-            r += (size_t) r1;
-            if (r1 == 0) {
+            R = read(d, &U[r], (size_t) (B - r));
+            r += R;
+            if (R == 0)
                 break;
-            }
         }
         if (r > 0) {
-            p(b, (size_t) r, o);
-            o += r > 0 ? (size_t) r : 0;
+            p(U, (size_t) r, O);
+            O += r > 0 ? (size_t) r : 0;
         }
     }
 
-    if (f) {
+    if (f)
         close(d);
-    }
 }
 
-int main(int c, char *v[]) {
-    if (c == 2 || c == 1) {
-        f(c == 1 ? NULL : v[1]);
-    } else {
-        exit(1);
-    }
+int main(int argc, char *argv[]) {
+    (argc == 2) ? h(argv[1]) : ((argc == 1) ? h(NULL) : exit(1));
     return 0;
 }
