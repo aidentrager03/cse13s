ea1d7ece2cc45a454df43d068f1dfb555254ea62
Aiden Trager <astrager@ucsc.edu>
2023-12-06 03:04:23 +0000

    new

--- asgn8/node.c
+++ asgn8/node.c
@@ -3,17 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node Node;
-
-struct Node {
-    uint8_t symbol;
-    uint32_t weight;
-    uint64_t code;
-    uint8_t code_length;
-    Node *left;
-    Node *right;
-};
-
 Node *node_create(uint8_t symbol, uint32_t weight) {
     Node *new_node = (Node *) malloc(sizeof(Node));
     if (new_node == NULL) {
--- asgn8/pq.c
+++ asgn8/pq.c
@@ -1,4 +1,3 @@
-// pq.c
 
 #include "pq.h"
 
